b3816909ca018f937f4b08cca3133c8e8f367bfd
dleqhuy <86642789+dleqhuy@users.noreply.github.com>
2022-12-29 18:01:55 +0700

    update

--- crnn/models.py
+++ crnn/models.py
@@ -2,43 +2,6 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
-def vgg_style(x):
-    """
-    The original feature extraction structure from CRNN paper.
-    Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
-    """
-    x = layers.Conv2D(
-        64, 3, padding='same', activation='relu', name='conv1')(x)
-    x = layers.MaxPool2D(pool_size=2, padding='same', name='pool1')(x)
-
-    x = layers.Conv2D(
-        128, 3, padding='same', activation='relu', name='conv2')(x)
-    x = layers.MaxPool2D(pool_size=2, padding='same', name='pool2')(x)
-
-    x = layers.Conv2D(256, 3, padding='same', use_bias=False, name='conv3')(x)
-    x = layers.BatchNormalization(name='bn3')(x)
-    x = layers.Activation('relu', name='relu3')(x)
-    # x = layers.Conv2D(
-    #     256, 3, padding='same', activation='relu', name='conv4')(x)
-    x = layers.MaxPool2D(
-        pool_size=2, strides=(2, 1), padding='same', name='pool4')(x)
-
-    x = layers.Conv2D(512, 3, padding='same', use_bias=False, name='conv5')(x)
-    x = layers.BatchNormalization(name='bn5')(x)
-    x = layers.Activation('relu', name='relu5')(x)
-    # x = layers.Conv2D(
-    #     512, 3, padding='same', activation='relu', name='conv6')(x)
-    x = layers.MaxPool2D(
-        pool_size=2, strides=(2, 1), padding='same', name='pool6')(x)
-
-    x = layers.Conv2D(512, 2, use_bias=False, name='conv7')(x)
-    x = layers.BatchNormalization(name='bn7')(x)
-    x = layers.Activation('relu', name='relu7')(x)
-
-    x = layers.Reshape((-1, 512), name='reshape7')(x)
-    return x
-
-
 def build_model(num_classes,
                 weight=None,
                 preprocess=None,
@@ -48,17 +11,41 @@ def build_model(num_classes,
     x = img_input = keras.Input(shape=img_shape)
     if preprocess is not None:
         x = preprocess(x)
-    
-    x = vgg_style(x)
-    # x = layers.Bidirectional(
-    #     layers.LSTM(units=256, return_sequences=True), name='bi_lstm1')(x)
 
-    x = layers.LSTM(units=256, return_sequences=True, name='bi_lstm1')(x)
+    # First conv block
+    x = layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        kernel_initializer="he_normal",
+        padding="same",
+        name="Conv1",
+    )(x)
+    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
 
-    # x = layers.Bidirectional(
-    #     layers.LSTM(units=256, return_sequences=True), name='bi_lstm2')(x)
-    
-    x = layers.LSTM(units=256, return_sequences=True, name='bi_lstm2')(x)
+    # Second conv block
+    x = layers.Conv2D(
+        64,
+        (3, 3),
+        activation="relu",
+        kernel_initializer="he_normal",
+        padding="same",
+        name="Conv2",
+    )(x)
+    x = layers.MaxPooling2D((2, 2), name="pool2")(x)
+
+    # We have used two max pool with pool size and strides 2.
+    # Hence, downsampled feature maps are 4x smaller. The number of
+    # filters in the last layer is 64. Reshape accordingly before
+    # passing the output to the RNN part of the model
+    new_shape = ((img_shape[0] // 4), (img_shape[1] // 4) * 64)
+    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
+    x = layers.Dense(64, activation="relu", name="dense1")(x)
+    x = layers.Dropout(0.2)(x)
+
+    # RNNs
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
+    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
 
     x = layers.Dense(units=num_classes, name='logits', dtype='float32')(x)
 
